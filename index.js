ffe838d8ae8f7c4d665ad96221f93f97162751c4
MuhammadAbdulAziz55 <abdulazizfrince5@gmail.com>
2023-03-02 08:37:03 +0600

    travel server

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
+const travelSpot = require("./data/TravelSpot.json");
+const travelDetailsInfo = require("./data/TravelDetailInfo.json");
 
 app.use(cors());
 
@@ -18,4 +20,29 @@ const hotels = require("./data/hotels.json");
 
 app.get("/hotels", (req, res) => {
   res.send(hotels);
+});
+
+// app.get("/hotels/:id", (req, res) => {
+//   const id = req.params.id;
+//   const hotel = hotels.find((ht) => ht.id === id);
+//   res.send(hotel);
+// });
+
+app.get("/hotels", (req, res) => {
+  res.send(hotels);
+});
+app.get("/travel-spot/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const tSpotHotel = hotels.filter((ht) => ht.spot_id === id);
+  res.send(tSpotHotel);
+});
+
+app.get("/travel-spot", (req, res) => {
+  res.send(travelSpot);
+});
+
+app.get("/travel-details-info/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedInfo = travelDetailsInfo.find((tdi) => tdi.id === id);
+  res.send(selectedInfo);
 });
